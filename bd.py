26500dff10bae42cf56ebb60bb60fa7c1705f348
David204123 <20232041@hmo.unidep.edu.mx>
2026-05-02 16:05:24 -0700

    Add files via upload

---
+++ bd.py
@@ -0,0 +1,10 @@
+recursos = {
+    1: {
+        "nome": "Recurso 1",
+        "descripcion": "Descripcion do recurso 1"
+    },
+    2: {
+        "nome": "Recurso 2",
+        "descripcion": "Descripcion do recurso 2"
+    },
+}
